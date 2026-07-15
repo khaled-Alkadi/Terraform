d1886ec2b0531e9a2878a5f2854e84bf8f3313f2
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-07-15 15:43:59 +0200

    Add resources to netowrking File:nsg, pub_ ip, nic, nsg associte

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -24,3 +24,45 @@ resource "azurerm_subnet" "Database" {
   virtual_network_name = azurerm_virtual_network.my-vnet.name
   address_prefixes = [ "10.0.3.0/24" ]
 }
+
+resource "azurerm_network_security_group" "terra_nsg" {
+  name = "nsg-edu-win-prod-01"
+  resource_group_name = azurerm_resource_group.my-group.name
+  location = azurerm_resource_group.my-group.location
+  tags = azurerm_resource_group.my-group.tags
+  security_rule {
+    name = "Allow-RDP-Only-From-My-IP"
+    priority = 100
+    protocol = "Tcp"
+    direction = "Inbound"
+    access = "Allow"
+    source_port_range = "*"
+    destination_port_range = "3389"
+    source_address_prefix = "84.115.213.195/32"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_public_ip" "pub_ip" {
+  name = "pub_ip_edu_server"
+  resource_group_name = azurerm_resource_group.my-group.name
+  location = azurerm_resource_group.my-group.location
+  allocation_method = "Static"
+  sku = "Standard"
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name = "nic-edu-win-prod-01"
+  resource_group_name = azurerm_resource_group.my-group.name
+  location = azurerm_resource_group.my-group.location
+  ip_configuration {
+    name = "internal"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id = azurerm_subnet.frontend_subnet.id
+    public_ip_address_id = azurerm_public_ip.pub_ip.id
+  }
+}
+resource "azurerm_network_interface_security_group_association" "nic_nsg_associte" {
+  network_interface_id = azurerm_network_interface.vm_nic.id
+  network_security_group_id = azurerm_network_security_group.terra_nsg.id
+  
+}
