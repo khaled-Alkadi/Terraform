75141946a5170a2f9de91d75e23afed9c7d090a3
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-07-16 14:47:03 +0200

    seperate porjects terraform

---
+++ terraform/lab01-basics/main.tf
@@ -0,0 +1,75 @@
+# 1. إعلام تيراموف بالتعامل مع سحابة Azure واختيار إصدار المزود
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # أو الإصدار الأحدث المستقر المتوفر
+    }
+  }
+  # كود رفع ملف الـ State وتأمينه في السحابة تلقائياً
+  backend "azurerm" {
+    resource_group_name  = "terraform-admin-rg"
+    storage_account_name = "stsharedtfstate01" # الاسم الذي اخترته في الأعلى
+    container_name       = "backend"
+    key                  = "lab01-basics.tfstate"
+  }
+}
+
+# 2. تفعيل الاتصال واستخدام الميزات (مهم جداً لربط الحساب الحساب الفعلي)
+# provider "azurerm" {
+#   features {}
+# }
+
+# resource "azurerm_resource_group" "my-group" {
+#   name     = "terraform-project"
+#   location = "westeurope"
+#   tags = {
+#     Envirenment = "Terraform"
+#     Owner       = "terra"
+#   }
+# }
+
+# # resource "azurerm_management_lock" "rg_lock" {
+# #   name = "rg-delete-lock"
+# #   scope = azurerm_resource_group.my-group.id
+# #   lock_level = "CanNotDelete"
+# #   notes = "delete lock"
+# # }
+
+# resource "azurerm_windows_virtual_machine" "edu_win_ser" {
+#   name                  = "edu-win-server"
+#   location              = azurerm_resource_group.my-group.location
+#   resource_group_name   = azurerm_resource_group.my-group.name
+#   admin_username        = "kha"
+#   admin_password        = data.azurerm_key_vault_secret.secret_education_server.value
+#   network_interface_ids = [azurerm_network_interface.vm_nic.id]
+#   size                  = "Standard_B2s"
+#   tags                  = azurerm_resource_group.my-group.tags
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2022-datacenter-smalldisk"
+#     version   = "latest"
+#   }
+# }
+
+# resource "azurerm_managed_disk" "data_disk" {
+#   name                 = "disk-edu-win-prod-01"
+#   create_option        = "Empty"
+#   location             = azurerm_resource_group.my-group.location
+#   resource_group_name  = azurerm_resource_group.my-group.name
+#   storage_account_type = "Standard_LRS"
+#   disk_size_gb         = 50
+# }
+
+# resource "azurerm_virtual_machine_data_disk_attachment" "disk_attach" {
+#   caching            = "ReadWrite"
+#   lun                = 10
+#   managed_disk_id    = azurerm_managed_disk.data_disk.id
+#   virtual_machine_id = azurerm_windows_virtual_machine.edu_win_ser.id
+
+# }
---
+++ terraform/lab01-basics/networking.tf
@@ -0,0 +1,68 @@
+# resource "azurerm_virtual_network" "my-vnet" {
+#   name                = "terra_vnet"
+#   location            = azurerm_resource_group.my-group.location
+#   resource_group_name = azurerm_resource_group.my-group.name
+#   address_space       = ["10.0.0.0/20"]
+
+#   tags = azurerm_resource_group.my-group.tags
+# }
+# resource "azurerm_subnet" "frontend_subnet" {
+#   name                 = "frontend_subnet"
+#   resource_group_name  = azurerm_resource_group.my-group.name
+#   virtual_network_name = azurerm_virtual_network.my-vnet.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
+# resource "azurerm_subnet" "backend" {
+#   name                 = "Backend"
+#   resource_group_name  = azurerm_resource_group.my-group.name
+#   virtual_network_name = azurerm_virtual_network.my-vnet.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
+# resource "azurerm_subnet" "Database" {
+#   name                 = "Database"
+#   resource_group_name  = azurerm_resource_group.my-group.name
+#   virtual_network_name = azurerm_virtual_network.my-vnet.name
+#   address_prefixes     = ["10.0.3.0/24"]
+# }
+
+# resource "azurerm_network_security_group" "terra_nsg" {
+#   name                = "nsg-edu-win-prod-01"
+#   resource_group_name = azurerm_resource_group.my-group.name
+#   location            = azurerm_resource_group.my-group.location
+#   tags                = azurerm_resource_group.my-group.tags
+#   security_rule {
+#     name                       = "Allow-RDP-Only-From-My-IP"
+#     priority                   = 100
+#     protocol                   = "Tcp"
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     source_port_range          = "*"
+#     destination_port_range     = "3389"
+#     source_address_prefix      = "84.115.213.195/32"
+#     destination_address_prefix = "*"
+#   }
+# }
+# resource "azurerm_public_ip" "pub_ip" {
+#   name                = "pub_ip_edu_server"
+#   resource_group_name = azurerm_resource_group.my-group.name
+#   location            = azurerm_resource_group.my-group.location
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
+
+# resource "azurerm_network_interface" "vm_nic" {
+#   name                = "nic-edu-win-prod-01"
+#   resource_group_name = azurerm_resource_group.my-group.name
+#   location            = azurerm_resource_group.my-group.location
+#   ip_configuration {
+#     name                          = "internal"
+#     private_ip_address_allocation = "Dynamic"
+#     subnet_id                     = azurerm_subnet.frontend_subnet.id
+#     public_ip_address_id          = azurerm_public_ip.pub_ip.id
+#   }
+# }
+# resource "azurerm_network_interface_security_group_association" "nic_nsg_associte" {
+#   network_interface_id      = azurerm_network_interface.vm_nic.id
+#   network_security_group_id = azurerm_network_security_group.terra_nsg.id
+
+# }
