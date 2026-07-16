ff5670a1f8f496e7f0d3939a43db49af7580fb0c
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-07-16 15:05:42 +0200

    labo1 update

--- terraform/lab01-basics/outputs.tf
+++ terraform/lab01-basics/outputs.tf
@@ -1,9 +1,9 @@
-# output "server_public_ip" {
-#   value = azurerm_public_ip.pub_ip.ip_address
-#   description = "The public IP address of the Windows Server"
-# }
+output "server_public_ip" {
+  value = azurerm_public_ip.pub_ip.ip_address
+  description = "The public IP address of the Windows Server"
+}
 
-# output "server_private_ip" {
-#   value = azurerm_network_interface.vm_nic.private_ip_address
-#   description = "The private IP address of the Windows Server"
-# }
+output "server_private_ip" {
+  value = azurerm_network_interface.vm_nic.private_ip_address
+  description = "The private IP address of the Windows Server"
+}
