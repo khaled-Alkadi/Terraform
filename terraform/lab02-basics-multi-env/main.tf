4e25a25e4e1112488bd80e5f6a19919ebc179eef
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-07-18 12:41:53 +0200

    add terra files for a new terraform project (multi-env)

---
+++ terraform/lab02-basics-multi-env/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0"
+    }
+    }
+    backend "azurerm" {
+      resource_group_name = "terraform-admin-rg"
+      storage_account_name = "stsharedtfstate01"
+      container_name = "backend"
+      key = "multi-env.tfstate"
+    }
+  }
+  provider "azurerm" {
+    features {}
+  }
+  resource "azurerm_resource_group" "multi_env_g" {
+  name = "terra-multi-env-rg"
+  location = "westeurope"
+  tags = {
+    env = "muli_env"
+    owner = "terra"
+    }
+  }
---
+++ terraform/lab02-basics-multi-env/security.tf
@@ -0,0 +1,32 @@
+resource "azurerm_network_security_group" "public_nsg" {
+  name = "nsg-public-web"
+  resource_group_name = azurerm_resource_group.multi_env_g.name
+  location = azurerm_resource_group.multi_env_g.location
+  tags = azurerm_resource_group.multi_env_g.tags
+}
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "AllowSSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  source_address_prefix       = "*"
+  destination_port_range      = "22"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.multi_env_g.name
+  network_security_group_name = azurerm_network_security_group.public_nsg.name
+}
+resource "azurerm_subnet_network_security_group_association" "public_assoc" {
+  network_security_group_id = azurerm_network_security_group.public_nsg.id
+  subnet_id = azurerm_subnet.public_subnet.id  
+}
+resource "azurerm_network_security_group" "private_nsg" {
+  name = "nsg-private-db"
+  resource_group_name = azurerm_resource_group.multi_env_g.name
+  location = azurerm_resource_group.multi_env_g.location
+}
+resource "azurerm_subnet_network_security_group_association" "private_assoc" {
+  network_security_group_id = azurerm_network_security_group.private_nsg.id
+  subnet_id = azurerm_subnet.private_subnet.id
+}
