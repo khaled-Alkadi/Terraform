72489fe6268238a2d68bfb021809a341b46adb95
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-07-28 02:36:36 +0200

    main update

--- terraform/azure-rbac-iac-secure-lab/main.tf
+++ terraform/azure-rbac-iac-secure-lab/main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
   backend "azurerm" {
-    resource_group_name = "value"
-    storage_account_name = "value"
-    container_name = "value"
-    key = "value"
+    resource_group_name  = "terraform-admin-rg"
+    storage_account_name = "stsharedtfstate01"
+    container_name       = "backend"
+    key                  = "azure-rbac-iac-secure-lab.tfstate"
   }
 }
 provider "azurerm" {
