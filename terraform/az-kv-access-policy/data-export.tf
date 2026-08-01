1e12e315ab040d42843013c08416674cb336f5d0
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-08-01 13:44:33 +0200

    access policy project is deleted

--- terraform/az-kv-access-policy/data-export.tf
+++
@@ -1,5 +0,0 @@
-# Entra ID:
-###########
-data "azurerm_client_config" "current_entra_id" {}
-# buckup resource group:
-########################
--- terraform/az-kv-access-policy/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "Entra_ID" {
-  value = data.azurerm_client_config.current_entra_id.value
-}
