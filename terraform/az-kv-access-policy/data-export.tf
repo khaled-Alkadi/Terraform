a7d489b66db8ecfa009d55aedb3a373d8d793fa1
khaled-Alkadi <khaled.alkadi@outlook.de>
2026-08-01 11:24:10 +0200

    az key vault access policies project

---
+++ terraform/az-kv-access-policy/data-export.tf
@@ -0,0 +1,5 @@
+# Entra ID:
+###########
+data "azurerm_client_config" "current_entra_id" {}
+# buckup resource group:
+########################
---
+++ terraform/az-kv-access-policy/outputs.tf
@@ -0,0 +1,3 @@
+output "Entra_ID" {
+  value = data.azurerm_client_config.current_entra_id.value
+}
